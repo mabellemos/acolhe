bd8f547ce93ad50c89ac8eddb1ad32bafef85f4a
mabellemos <mils2@aluno.ifal.edu.br>
2024-07-14 22:51:22 -0300

    Create architecture directiories

--- src/main/java/io/github/mabellemos/acolheapi/Main.java
+++ src/main/java/io/github/mabellemos/acolheapi/Main.java
@@ -5,17 +5,33 @@ import io.github.mabellemos.acolheapi.core.domain.Clothes;
 import io.github.mabellemos.acolheapi.core.domain.Responsible;
 import io.github.mabellemos.acolheapi.core.domain.Shelters;
 
+import java.util.Scanner;
 import java.util.UUID;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       UUID id = UUID.randomUUID();
 
-       Clothes end = new Clothes(id);
-       System.out.println(end.toString());
-       end.alter(id);
-       System.out.println(end.toString());
+        Scanner input = new Scanner(System.in);
+        String opc;
+        do {
+            System.out.println("ACOLHE");
+            System.out.println("1 - Doações\n2 - Centros de Distribuição");
+            int esc = input.nextInt();
+
+            switch (esc){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+            System.out.println("Deseja realizar outra operação? (Sim/Não)");
+            opc = input.next();
+
+        } while (opc.equalsIgnoreCase("sim"));
     }
 }
